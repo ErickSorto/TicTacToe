981695f9756a0cf71a8b968e2380bf4dde3f9f82
Erick Sorto <SortoErick33@gmail.com>
2021-07-27 23:51:40 -0400

    Tic tac toe

---
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.tictactoe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    private int hi = 30;
+    private boolean
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
